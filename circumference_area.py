fe8121aa6223f82a6eb855abf9b1c75c8decaf49
clexa16 <ruhsuzclexa@gmail.com>
2026-08-14 12:12:37 +0300

    Add files via upload

---
+++ circumference_area.py
@@ -0,0 +1,6 @@
+pi=3.14159
+radius_of_circle=float(input("Radius:"))
+area_of_circle=pi*radius_of_circle*radius_of_circle
+circumference_of_circle=2*pi*radius_of_circle
+print("Area:",area_of_circle)
+print("Circumference:",circumference_of_circle)
