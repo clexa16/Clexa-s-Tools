b1d0e99ee7942dfd3db03e09f8d77ad100a3e959
clexa16 <ruhsuzclexa@gmail.com>
2026-08-14 20:09:00 +0300

    Add files via upload

---
+++ basic_name_checker.py
@@ -0,0 +1,5 @@
+Nick=str(input("Please enter your name:"))
+if len(Nick) < 3 or len(Nick) >12:
+    print("Warning! Please Try Another Name! Your name must be longert than 3 characters and shorter than 12 characters ")
+else:
+    print("Your Name İs Suitable Entering System")
