4d1f6921b4ce1bf17a5761a35c407b4139d93896
clexa16 <ruhsuzclexa@gmail.com>
2026-08-14 20:31:20 +0300

    Add files via upload

---
+++ advanced_calculator.py
@@ -0,0 +1,12 @@
+process=input("Please Enter Process You Wanted(+,/,*,-) ")
+number_1=float(input("Please Enter First Number:"))
+number_2=float(input("Please Enter Second Number:"))
+
+if process == "+":
+    print(number_1+number_2)
+if process == "/":
+    print(number_1/number_2)
+if process == "*":
+    print(number_1*number_2)
+if process == "-":
+    print(number_1-number_2)
